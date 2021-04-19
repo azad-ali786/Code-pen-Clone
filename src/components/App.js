809f5a5246b5626f6d27950cc009850b1b0f27f9
azad-ali786 <azad_ug@cse.nits.ac.in>
2021-04-20 00:32:50 +0530

    Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,23 @@
-
+import React from 'react'
+import Editor from './Editor'
 function App() {
-  return 'Hello';
+  return (
+    <>
+    <div className="pane top-pane">
+      <Editor/>
+      <Editor/>
+      <Editor/>
+    </div>
+    <div className="pane">
+      <iframe title="output"
+       sandbox="allow-scripts"
+       frameborder="0"
+       width="100%"
+       height="100%"
+       />
+    </div>
+    </>
+  );
 }
 
 export default App;
