e8a0c438cad195a00eccc9888718062e4947701e
azad-ali786 <azad_ug@cse.nits.ac.in>
2021-04-20 01:38:23 +0530

    Transition and Icons added

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
 import { Controlled as ControlledEditor } from "react-codemirror2";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faCompressAlt,faExpandAlt} from "@fortawesome/free-solid-svg-icons"
 const Editor = (props) => {
   const { language, displayName, value, onChange } = props;
   function handleChange(editor, data, value) {
     onChange(value);
   }
+
+  const [open, setOpen] = useState(true);
+  console.log(open);
+
   return (
-    <div className="editor-container">
+    <div className={`editor-container ${open ? '' : 'collapsed '}`}>
       <div className="editor-title">
         {displayName}
-        <button>0/C</button>
+        <button
+         type="button"
+         className="expand-collapse-btn"
+         onClick={() => setOpen((prevOpen) => !prevOpen)}>
+            <FontAwesomeIcon icon={open ? faCompressAlt:faExpandAlt} />
+        </button>
       </div>
       <ControlledEditor
         onBeforeChange={handleChange}
@@ -23,7 +34,7 @@ const Editor = (props) => {
         options={{
           lineWrapping: true,
           mode: language,
-          theme:'material',
+          theme: "material",
           lineNumbers: true,
         }}
       />
